6793281eb92e5fd1129ef0bc27000dee64bb99e9
MuhdAbbasHamdard <muhdabbaskan@gmail.com>
2026-05-23 16:46:08 +0430

    accept changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import Contact from "./components/Contact";
 import Vision from "./components/Vision";
 import Teaching from "./components/Teaching";
 import Projects from "./components/Projects";
-<<<<<<< HEAD
-=======
 import Testimonials from "./components/Testimonials";
-// import CursorDot from "./components/CursorDot";
-// import { Toaster } from "./components/Toaster";
->>>>>>> 84f9ec30c30523686f60a84b10251feacec47f99
 
 function App() {
   return (
